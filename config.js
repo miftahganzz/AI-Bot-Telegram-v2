ba8f064c2a5ad3ffdec21a2898c1caeb03e756c4
Miftah GanzZ <138507664+miftahganzz@users.noreply.github.com>
2023-10-14 11:56:09 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  botToken: '6064499783:AAFE5igifDGWLTRShyzm6H_P2D9oora4fRY', //bot token dari bot father
+//============================================
+  miftahkey: 'miftah', // apikey api-miftah.xyz
+  miftahkey2: 'miftah', // Apikey api.miftahganzz.my.id (default: free or global)
+  apimiftah: 'https://api-miftah.xyz',
+  apimiftah2: 'https://api.miftahganzz.my.id',
+//============================================
+  langsimi: 'id',
+  versi: '2.1.0', // versi bot
+  prefix: '/',
+  groupChatId: '-1001965556417',
+  grupUrl: 'https://t.me/miftahgrup',
+//============================================
+  web1: 'https://s.id/miftahbotz',
+  web2: 'https://api-miftah.xyz',
+//============================================
+  ownerContact: {
+    ownerChatID: '5675049989', // id tele owner
+    username: 'Miftah GanzZ',
+    phoneNumber: '6282219148083', //nomor tele 
+    firstName: 'Miftah', //nama depan
+    lastName: 'GanzZ', // nama belakang
+  },
+//============================================
+  qrisImage: './qris.jpg', //gambar qris buat donasi
+};
